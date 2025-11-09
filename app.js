8641834a42a43d49e72d17afb238d470bab61688
Dhia Ben Bouali <159145021+Dhia-Ben-Bouali@users.noreply.github.com>
2025-11-09 17:33:03 +0100

    7

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const routes = require('./routes');
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(3000, () => {
-  console.log('App running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
